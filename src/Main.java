db87c23814faed06fa1a4369a95cfaeaadc4104c
Nicole Omadova Robertson <nicole.omadovarobertson@hm.com>
2021-04-09 14:15:40 +0200

    Added a method2 that decrements the input argument

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(method(8));
+        System.out.println(method2(16));
 
     }
 
     public static int method (int a){
         return a*2+3;
+    }
+
+    public static int method2 (int a){
+        return a/2-1;
     }
 }

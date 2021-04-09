1cf773e1a32523e475ef490a4ac65e175c7fd996
Nicole Omadova Robertson <nicole.omadovarobertson@hm.com>
2021-04-09 11:55:22 +0200

    Testing exercise 3 by adding a method into the git project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(method(8));
+
+    }
+
+    public static int method (int a){
+        return a*2+3;
+    }
+}

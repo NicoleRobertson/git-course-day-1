ad6c4eb3f9fec7a6eb8acd4b6aca5a038a542b05
Nicole Omadova Robertson <nicole.omadovarobertson@hm.com>
2021-04-09 15:30:59 +0200

    Added metod3 to main

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,16 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(method(8));
+        System.out.println(method3(8, 9));
 
     }
 
     public static int method (int a){
         return a*2+3;
     }
+
+    public static int method3 (int a, int b){
+        return a*b + 22;
+    }
+
 }
